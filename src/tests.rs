78ba3a9d6029f688a4897009e7af69a0c25160fd
leto <justinpraxis@gmail.com>
2020-08-30 01:07:01 +0300

    get rid of box in test_suite

--- src/tests.rs
+++ src/tests.rs
@@ -5,7 +5,7 @@ mod tests {
     use rand::Rng;
     use std::cmp::Ordering;
 
-    fn test_suite(sort_fn: impl Fn(&mut [i32], Box<dyn Fn(&i32, &i32) -> Ordering>) -> Vec<i32>) {
+    fn test_suite(sort_fn: impl Fn(&mut [i32], &dyn Fn(&i32, &i32) -> Ordering) -> Vec<i32>) {
         // generate tests like https://github.com/rust-lang/rust/blob/847ba835ce411d47364a93ddf0b4a5c0f27928a9/library/alloc/tests/slice.rs
         let rng = thread_rng();
         for len in (2..25).chain(500..510) {
@@ -19,12 +19,12 @@ mod tests {
 
                     let mut v = orig.clone();
                     let cmp = |a: &i32, b: &i32| -> Ordering { a.cmp(b) };
-                    let sorted = sort_fn(&mut v, Box::new(cmp));
+                    let sorted = sort_fn(&mut v, &cmp);
                     assert!(sorted.windows(2).all(|w| w[0] <= w[1]));
 
                     let mut v = orig.clone();
                     let cmp = |a: &i32, b: &i32| -> Ordering { b.cmp(a) };
-                    let sorted = sort_fn(&mut v, Box::new(cmp));
+                    let sorted = sort_fn(&mut v, &cmp);
                     assert!(sorted.windows(2).all(|w| w[0] >= w[1]));
                 }
             }
